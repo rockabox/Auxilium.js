616e2ee816fb5b750356deedc83f10c283da450b
Nick White <nick.white@rockabox.com>
2014-10-31 15:53:01 +0000

    Add JSDoc info for loadJson

--- src/utils/load-json.js
+++ src/utils/load-json.js
@@ -1,3 +1,15 @@
+/**
+ * Loads a JSON File and passes the content to a callback function
+ * @param {string}   jsonFile The location of the JSON file in which to load.
+ * @param {Function} callback A function in which to run on completing teh load of the file.
+ *
+ * @example
+ *  var file = 'http://example.com/pokemon.json',
+ * 		callback = function (response) {
+ * 			console.log(response);
+ * 		};
+ * // Will console.log the entire contents of the pokemon.json file loaded.
+ */
 function loadJson (jsonFile, callback) {
     var xobj = new XMLHttpRequest();
 
