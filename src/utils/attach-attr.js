4a837b9ec7956227d9c856181de4b3b3e5b60737
Nick White <nick.white@rockabox.com>
2014-10-31 15:48:15 +0000

    Add JSDoc info for attachAttr

--- src/utils/attach-attr.js
+++ src/utils/attach-attr.js
@@ -1,6 +1,22 @@
-function attachAttr (ele, attr) {
-    for (var a in attr) {
-        ele.setAttribute(a, attr[a]);
+/**
+ * Attaches DOM attribles to an elements
+ * @param {object} ele    A DOM Element in which to add the CSS to.
+ * @param {object} attrObj Attributes in which to add to the Element in an object
+ * @returns {object} ele  Passes the element with attributes attached.
+ *
+ * @example
+ *  var x = document.createElement('img'),
+ * 		attr = {
+ * 			'src': 'http://example.com/something.gif',
+ *    		'width': '100',
+ *    		'height': '200'
+ *   	};
+ *  // Returns x (<img src="http://example.com/something.gif" width="100" height="200" />)
+ */
+function attachAttr (ele, attrObj) {
+    for (var attr in attrObj) {
+        ele.setAttribute(attr, attrObj[attr]);
     }
+
     return ele;
 }
