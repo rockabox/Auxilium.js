b00e74f71e9d0bb12cb2e7497c21a1162ebcb759
Nick White <nick.white@rockabox.com>
2014-10-31 15:38:37 +0000

    Adds JSDoc info for attachCSS

--- src/utils/attach-css.js
+++ src/utils/attach-css.js
@@ -1,7 +1,24 @@
-function attachCss (ele, css) {
-    var s = ele.style;
-    for (var c in css) {
-        s[c] = css[c];
+/**
+ * Attaches CSS to an elements style attribute
+ * @param {object} ele    A DOM Element in which to add the CSS to.
+ * @param {object} cssObj CSS in which to add to the Element in an object
+ * @returns {object} ele  Passes the element with css attached to style attribute.
+ *
+ * @example
+ *  var div = document.createElement('div'),
+ * 		css = {
+ * 			'position': 'relative',
+ *    		'backgroundColor': 'black'
+ *   	};
+ *  attachCss(ele, css);
+ *  // Returns x (<div style="position: relative; background-colour: black"></div>)
+ */
+function attachCss (ele, cssObj) {
+    var style = ele.style;
+
+    for (var css in cssObj) {
+        style[css] = cssObj[css];
     }
+
     return ele;
 }
