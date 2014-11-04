aa7f63fac2e364c53f25dc567fccedcc4df93429
Nick White <nick.white@rockabox.com>
2014-11-04 16:11:31 +0000

    Set up of JS Hooks

---
+++ src/utils/js-hooks.js
@@ -0,0 +1,32 @@
+/**
+ * Rockabox JSHook class, it will setup custom hooks set in the Placement js
+ * for Load, Init, View initial, Engagement and Click.
+ *
+ * @param {object} advert      The Advert class
+ * @param {object} customHooks Custom hooks set within Placement js
+ */
+function jsHooks (advert, customHooks) {
+    var $this = this,
+        events = advert.utils.events,
+        hooks = [
+            'load',
+            'init',
+            'viewInitial',
+            'engagement',
+            'click'
+        ];
+
+    /**
+     * Initializes the js hooks
+     */
+    function init () {
+        for (var hook in hooks) {
+            var hookName = hooks[hook],
+                hookEvent = 'rb.' + hookName;
+
+            events.addListener(advert.getCreative(), hookEvent, customHooks[hookName]);
+        }
+    }
+
+    init();
+}
