e2cfb3067063939fbfbfbcbe013a9fced5b7eb84
jonman <jon.man@rockabox.com>
2014-12-03 10:50:03 +0000

    Couple of passing unit tests.

--- src/utils/serialize.js
+++ src/utils/serialize.js
@@ -1,31 +1,35 @@
-/**
- * Serialize's an object into query params for a URL
- * Passes & between params (? or & not prefixed)
- * @param  {object} queryParams The different params to serialize (key becomes &param value becomes =value)
- * @return {string}        Passes back a serialized version from the object ready to be used for a URL
- *
- * @example
- *  var queryParams = {
- *  		'first_name': 'Bart',
- *  		'last_name': 'Simpson'
- * 		};
- * 	serialize(queryParams);
- * 	// returns
- * 	// first_name=Bart&last_name=Simpson
- */
-function serialize (queryParams) {
-    var str = [],
-        key,
-        value,
-        query;
+define([], function () {
+    /**
+     * Serialize's an object into query params for a URL
+     * Passes & between params (? or & not prefixed)
+     * @param  {object} queryParams The different params to serialize (key becomes &param value becomes =value)
+     * @return {string}        Passes back a serialized version from the object ready to be used for a URL
+     *
+     * @example
+     *  var queryParams = {
+     *  		'first_name': 'Bart',
+     *  		'last_name': 'Simpson'
+     * 		};
+     * 	serialize(queryParams);
+     * 	// returns
+     * 	// first_name=Bart&last_name=Simpson
+     */
+    function serialize (queryParams) {
+        var str = [],
+            key,
+            value,
+            query;
 
-    for (query in queryParams) {
-        if (queryParams.hasOwnProperty(query) && queryParams[query]) {
-            key = encodeURIComponent(query);
-            value = encodeURIComponent(queryParams[query]);
-            str.push(key + '=' + value);
+        for (query in queryParams) {
+            if (queryParams.hasOwnProperty(query) && queryParams[query]) {
+                key = encodeURIComponent(query);
+                value = encodeURIComponent(queryParams[query]);
+                str.push(key + '=' + value);
+            }
         }
+
+        return str.join('&');
     }
 
-    return str.join('&');
-}
+    return serialize;
+});
