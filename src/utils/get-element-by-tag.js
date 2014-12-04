b88bdbeb6ae81def06de483469941ee5c571b910
Nick White <nick.white@rockabox.com>
2014-12-04 12:59:53 +0000

    JSDoc fix

--- src/utils/get-element-by-tag.js
+++ src/utils/get-element-by-tag.js
@@ -2,7 +2,7 @@ define([], function () {
     /**
      * Get all elements with a certain tag name within an element and fire a callback
      * @param {object}   ele      The containing object in which to search within
-     * @param {[type]}   tag      The type of tag name in which should be looked for
+     * @param {string}   tag      The type of tag name in which should be looked for
      * @param {Function} callback A function in which to be fired being passed the element found
      *
      * @returns All of the elements found with the tag specified inside the ele
