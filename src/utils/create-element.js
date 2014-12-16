d9677102c0344a7450c30958f6f11668989a751f
Nick White <nick.white@rockabox.com>
2014-12-16 19:09:28 +0000

    JSDoc info added for createElement module

--- src/utils/create-element.js
+++ src/utils/create-element.js
@@ -9,7 +9,7 @@ define([
      * @param {string} tag                      The HTML tag type in which to create
      * @param {object} params                   Contains paramaters to be used for the creation of the element
      * @param {object} params.attr              Contains the HTML node attributes and it's values to be added to the
-     *                                          element
+     *                                          element (should not pass class through here use cssNames).
      * @param {object} params.css               Contains the css styling to be added to the element
      * @param {string|array} params.cssNames    Contains css class name or names in which to attach to an element
      * @param {object} params.events            Contains event handlers to be attached to an element
@@ -18,6 +18,28 @@ define([
      * @requires attachAttr
      * @requires attachCss
      * @requires attachClass
+     *
+     * @example
+     * var params = {
+     * 		attr: {
+     * 			src: 'http://rockabox.com/example.gif'
+     * 		},
+     * 		events: {
+     * 			load: function () {
+     * 				console.log('Fire me when the image is loaded');
+     * 			}
+     * 		},
+     * 		css: {
+     * 			border: '1px solid black',
+     * 			backgroundColor: 'red'
+     * 		},
+     * 		cssNames: ['legen', 'wait-for-it', 'dary']
+     * };
+     * var imageEle = createElement('img', params);
+     * // Returns
+     * // <img src="http://rockabox.com/example.gif" style="border: 1px solid black; background-color: red;"
+     * //   class="legen wait-for-it dary" />
+     * // When the image source has loaded a function will be ran console logging out.
      *
      * @returns ele The HTML element created with css and attributes added to passed from params
      */
