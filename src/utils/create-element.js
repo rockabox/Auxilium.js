b349359ed74806a09cc4bcf17fc01f7e81f6a105
Nick White <nick.white@rockabox.com>
2014-12-16 12:36:52 +0000

    Use attach class in create

--- src/utils/create-element.js
+++ src/utils/create-element.js
@@ -1,8 +1,9 @@
 define([
     'utils/attach-attr',
+    'utils/attach-class',
     'utils/attach-css',
     'utils/attach-events'
-], function (attachAttr, attachCss, attachEvents) {
+], function (attachAttr, attachClass, attachCss, attachEvents) {
     /**
      * Creates an element and returns the element
      * @param {string} tag              The HTML tag type in which to create
@@ -10,6 +11,7 @@ define([
      * @param {object} params.attr      Contains the HTML node attributes and it's values to be added to the element
      * @param {object} params.css       Contains the css styling to be added to the element
      * @param {object} params.events    Contains event handlers to be attached to an element
+     * @param {string} params.className Contains a class name in which to attach to an element
      *
      * @returns ele The HTML element created with css and attributes added to passed from params
      */
@@ -25,6 +27,7 @@ define([
         attachEvents(ele, params.events);
         attachAttr(ele, params.attr);
         attachCss(ele, params.css);
+        attachClass(ele, params.className);
 
         return ele;
     }
