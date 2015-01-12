d4cc0b308e5cea59c7d723e5c9f74967a56884f4
Nick White <nick.white@rockabox.com>
2015-01-12 15:49:27 +0000

    Adds example for hasProperty util

--- src/utils/has-property.js
+++ src/utils/has-property.js
@@ -2,13 +2,30 @@
  * A module that checks whether an object has the given properties.
  * @module utils/has-property
  */
-define([], function () {
+define([
+], function () {
     /**
      * hasProperty
      * @memberOf module:utils/has-property
      * @param {Object} obj Object to check
      * @param {String} path Properties to check
+     *
      * @return {Boolean} True if the given object contains the properties
+     *
+     * @example
+     * var testObj = {
+     * 		'cartoons': {
+     * 			'simpsons': {
+     * 				'Barney': 'I am here!'
+     * 			}
+     * 		}
+     * };
+     *
+     * // Returns true (that Barney key exists on the simpons object key).
+     * hasProperty(testObj, 'cartoons.simpsons.Barney');
+     *
+     * // Returns false (that Ash key does not exist on the simpons object key).
+     * hasProperty(testObj, 'cartoons.simpsons.Ash');
      */
     function hasProperty (obj, path) {
         var args = path.split('.');
