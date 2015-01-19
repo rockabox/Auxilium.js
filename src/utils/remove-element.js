cb7e835a30c1daef87d1ec6cae0d0d84777a5ed6
jonman <jon.man@rockabox.com>
2015-01-19 13:05:47 +0000

    Jsdoc for remove element util

--- src/utils/remove-element.js
+++ src/utils/remove-element.js
@@ -1,7 +1,16 @@
+/**
+ * A module which removes all child nodes and optionally, the main node.
+ * @module utils/remove-element
+ */
 define([
-
 ], function () {
-
+    /**
+     * Removes an elements child node
+     * @memberOf module:utils/remove-element
+     * @param {Object} node HTML node to act upon
+     * @param {Boolean:false} removeParent Optional: remove the entire node
+     * return {Object} Original HTML node
+     */
     function removeElement (node, removeParent) {
         var parent = node.parentNode;
         while (node.hasChildNodes()) {
