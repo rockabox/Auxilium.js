f6fca2bccd152354e424ad47c089d610ea86a5b6
Nick White <nick.white@rockabox.com>
2015-02-04 13:21:08 +0000

    Run karma tests locally

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('test', [
-        'lint'
+        'lint',
+        'karma:unit'
     ]);
 };
--- karma.conf.js
+++ karma.conf.js
@@ -3,33 +3,46 @@ var webpackConf = require('./webpack.config.js'),
     opts = lodash.cloneDeep(webpackConf);
 
 module.exports = function (config) {
-    config.set({
-        basePath: './',
-        files: [
-            'tests/helpers/**/*.matchers.js',
-            'tests/**/*.spec.js'
-        ],
-        browsers: [
-            'Chrome',
-            'Firefox'
-        ],
-        frameworks: [
-            'jasmine'
-        ],
-        port: 9876,
-        runnerPort: 9100,
-        singleRun: true,
-        captureTimeout: 60000,
-        reporters: [
-            'progress'
-        ],
-        preprocessors: {
-            'src/**/*.js': 'coverage',
-            'tests/**/*.spec.js': 'webpack'
-        },
-        coverageReporter: {
-            dir: '.reports/js/coverage/'
-        },
-        webpack: opts
-    });
+    var configuration = {
+            customLaunchers: {
+              'Chrome_travis_ci': {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+              }
+            },
+            basePath: './',
+            files: [
+                'tests/helpers/**/*.matchers.js',
+                'tests/**/*.spec.js'
+            ],
+            browsers: [
+                'Chrome',
+                'Firefox'
+            ],
+            frameworks: [
+                'jasmine'
+            ],
+            port: 9876,
+            runnerPort: 9100,
+            singleRun: true,
+            captureTimeout: 60000,
+            reporters: [
+                'progress'
+            ],
+            preprocessors: {
+                'src/**/*.js': 'coverage',
+                'tests/**/*.spec.js': 'webpack'
+            },
+            coverageReporter: {
+                dir: '.reports/js/coverage/'
+            },
+            webpack: opts
+        };
+
+    if (process.env.TRAVIS) {
+        // If we're running on Travis CI use the custom launcher
+        configuration.browsers = ['Chrome_travis_ci', 'Firefox'];
+    }
+
+    config.set(configuration);
 };
