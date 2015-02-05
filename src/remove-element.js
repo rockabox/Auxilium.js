7ed84e8ece06e843ae6f25a5035fa00e3a7acc7e
Nick White <nick.white@rockabox.com>
2015-02-05 19:18:05 +0000

    Add example for remove-element

--- src/remove-element.js
+++ src/remove-element.js
@@ -9,6 +9,26 @@ define([
      * @param {Boolean} self Optional: Remove the entire node
      *
      * return {Object} Original HTML node
+     *
+     * @example
+     * ```html
+     * <body>
+     * <div class="my-parent">
+     * 		<div>
+     * 			<span></span>
+     * 	 	</div>
+     * 		<img />
+     *  </div>
+     * </body>
+     * ```
+     * ```js
+     * removeElement(myParent);
+     * // Returns <body> (containing <div class="my-parent"></div>)
+     * ```
+     * ```js
+     * removeElement(myParent, true)
+     * // Returns <body></body (no longer containing <div class="my-parent"></div>)
+     * ```
      */
     function removeElement (node, self) {
         var parent = node.parentNode;
