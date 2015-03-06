100476c4c6a0c7530b5bf711287aced1d2b46611
Nick White <nick.white@rockabox.com>
2015-03-06 15:34:48 +0000

    Use is defined to check is a function not has property

--- src/console-logger.js
+++ src/console-logger.js
@@ -1,23 +1,24 @@
 define([
-    'aux/has-property'
-], function (hasProperty) {
+    'aux/is-defined'
+], function (isDefined) {
     /**
      * Using the browsers console logging options, allowing to use console.error if accessible otherwise fallsback to
      * console.log (again if accessible).
      *
      * @exports console-logger
      *
-     * @requires module:has-property
+     * @requires module:is-defined
      *
      * @param {*} message Message to log out
      * @param {String} type What logging system to use error, log (defaults to log).
      */
     function logger (message, type) {
+        var console = window.console;
         type = type || 'log';
 
-        if (hasProperty(window, 'console.' + type)) {
+        if (isDefined(console[type], 'function')) {
             console[type](message);
-        } else if (hasProperty(window, 'console.log')) {
+        } else if (isDefined(console.log, 'function')) {
             console.log(message);
         }
     }
