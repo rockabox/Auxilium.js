b9f7a4614d66c27020b2b9225ee80c75386a5720
Nick White <nick.white@rockabox.com>
2015-03-26 19:11:08 +0000

    Create elements with contents and children
    Adds on to the current createElement module the possiblity of creating an element with:
    - innerHTML (A string representation of contents for an element)
    - children (Pre-created HTMLDOMNodes in which to append to the element when created)
    - innerElements (Create new HTMLDOMNodes when creating the element and append -- recursively)

--- tests/create-element.spec.js
+++ tests/create-element.spec.js
@@ -1,6 +1,8 @@
 define([
-    'aux/create-element'
-], function (createElement) {
+    'aux/create-element',
+    'aux/get-elements-by-class',
+    'aux/has-class'
+], function (createElement, getElementsByClass, hasClass) {
     describe('Creating a DOM element', function () {
 
         it('should create an element with no styling or attributes', function () {
@@ -44,6 +46,64 @@ define([
             ele.onclick();
 
             expect(counter).toBe(1);
+        });
+
+        describe('Creating elements with child elements', function () {
+            var params,
+                ele;
+
+            beforeEach(function () {
+                params = {
+                    innerElements: [
+                        {
+                            tag: 'div',
+                            cssNames: 'child-one',
+                            innerElements: [
+                                {
+                                    tag: 'span',
+                                    cssNames: 'child-two'
+                                }
+                            ]
+                        }
+                    ]
+                };
+
+                ele = createElement('div', params);
+            });
+
+            it('should create child elements', function () {
+                expect(getElementsByClass(ele, 'child-one').length).toBe(1);
+            });
+
+            it('should recurssively create elements (a childs children)', function () {
+                var eleTwo = getElementsByClass(ele, 'child-two');
+
+                expect(eleTwo.length).toBe(1);
+                expect(hasClass(eleTwo[0].parentNode, 'child-one')).toBeTruthy();
+            });
+        });
+
+        it('should add text as inner HTML', function () {
+            var params = {
+                    innerHTML: '<div class="something"><span class="something"></span></div>'
+                },
+                ele = createElement('div', params);
+
+            expect(getElementsByClass(ele, 'something').length).toBe(2);
+        });
+
+        it('should add pre-created elements as children when creating the element', function () {
+            var childEle = createElement('div', {
+                    cssNames: 'jazz'
+                }),
+                params = {
+                    children: [childEle]
+                },
+                ele = createElement('div', params),
+                getEle = getElementsByClass(ele, 'jazz');
+
+            expect(getEle.length).toBe(1);
+            expect(getEle[0]).toBe(childEle);
         });
     });
 });
