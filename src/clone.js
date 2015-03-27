90c1ccad58171a6a260fee4a055e24041c05ff3b
jonman <jon.man@rockabox.com>
2015-03-27 11:19:13 +0000

    Fix cloning of arrays

--- src/clone.js
+++ src/clone.js
@@ -1,16 +1,20 @@
 define([
-], function () {
+    'aux/is-defined'
+], function (isDefined) {
     /**
      * Clones an object, returning a new instance of the object passed.
      *
      * @exports clone
+     *
+     * @requires module:is-defined
      *
      * @param  {object} obj The base object which to make a fresh copy of
      *
      * @return {object}     A fresh instance of the object originally passed
      */
     function clone (obj) {
-        if (obj === null || typeof obj !== 'object') {
+        if (obj === null || !isDefined(obj, 'object')) {
+        // if (obj === null || typeof obj !== 'object') {
             return obj;
         }
 
