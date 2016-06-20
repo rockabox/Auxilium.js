9750623bfb03c539348d0013db48f6731908e307
Robert Węglarek <weglarek.robert@gmail.com>
2016-06-20 14:59:36 +0100

    Add isTrusted method to check events

---
+++ tests/is-trusted.spec.js
@@ -0,0 +1,44 @@
+define([
+    'aux/is-trusted'
+], function (isTrusted) {
+
+    fdescribe('Will check whether or not an event', function () {
+
+        describe('is trusted', function () {
+            it('should return that an event is trusted when isTrusted defined', function () {
+                var event = {isTrusted: true};
+
+                expect(isTrusted(event)).toBeTruthy();
+            });
+
+            it('should return that an event is trusted when valid clientX/clientY defined', function () {
+                var event = {clientX: 10, clientY: 1};
+
+                expect(isTrusted(event)).toBeTruthy();
+            });
+        });
+
+        describe('is not trusted', function () {
+            it('should return false if isTrusted set to false', function () {
+                var event = {isTrusted: false};
+
+                expect(isTrusted(event)).toBeFalsy();
+            });
+
+            it('should return false if clientX and clientY set to 0', function () {
+                var event = {clientX: 0, clientY: 0};
+
+                expect(isTrusted(event)).toBeFalsy();
+            });
+
+            it('should return false if missing clientX or clientY', function () {
+                var eventNoX = {clientY: 10},
+                    eventNoY = {clientX: 10};
+
+                expect(isTrusted(eventNoX)).toBeFalsy();
+                expect(isTrusted(eventNoY)).toBeFalsy();
+            });
+        });
+    });
+
+});
