2a8a8f446fcffa2f5877885167f900afb262b4bd
Nick White <nick.white@scoota.com>
2017-02-24 15:32:30 +0000

    Fixes a bug where zero would not be serialized.

--- tests/serialize.spec.js
+++ tests/serialize.spec.js
@@ -4,13 +4,22 @@ define([
     describe('serialize', function () {
         it('should correctly serializes object to query string', function () {
             var query = serialize({
-                    dodgy: '?^%&$£',
-                    more: '%%{{}}',
-                    special: '你好世界',
-                    characters: 'áéíóúüñ¿¡'
-                });
+                dodgy: '?^%&$£',
+                more: '%%{{}}',
+                special: '你好世界',
+                characters: 'áéíóúüñ¿¡'
+            });
 
             expect(query.split('&').length).toEqual(4);
+        });
+
+        it('should allow for a zero to be attached to the query string', function () {
+            var query = serialize({
+                something: 'random',
+                dwell: 0
+            });
+
+            expect(query).toContain('something=random&dwell=0');
         });
     });
 });
